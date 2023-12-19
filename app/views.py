8727dadfd452d8a4c2770fe5c0fca6bfffdb8b1d
arun30052003 <arundpi6@gmail.com>
2023-12-20 00:11:49 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -6,11 +6,8 @@ from django.db.models.functions import Length
 
 def dept(request):
     QLTO=Dept.objects.all()
-    QLTO=Dept.objects.all().order_by('deptno' and Length('dname').desc())
-    QLTO=Dept.objects.all().order_by('-deptno')
-    QLTO=Dept.objects.all().order_by(Length('deptno'))
-    QLTO=Dept.objects.all().order_by(Length('deptno').desc())
-    QLTO=Dept.objects.all().order_by('deptno')[::]
+    QLTO=Dept.objects.all().order_by(Length('dname').desc())
+    QLTO=Dept.objects.filter(deptno__in=('1','4'))
     
     d={'Dept':QLTO}
     return render(request,'dept.html',d)
@@ -22,6 +19,18 @@ def emp(request):
     QLTO=Emp.objects.all().order_by(Length('ename'))
     QLTO=Emp.objects.all().order_by(Length('ename').desc())
     QLTO=Emp.objects.all().order_by('empno')[::]
+    QLTO=Emp.objects.exclude(ename='sivangi')
+    QLTO=Emp.objects.filter(Hiredate__month=5)
+    QLTO=Emp.objects.filter(Hiredate__year=2023)
+    QLTO=Emp.objects.filter(Hiredate__day=10)
+    QLTO=Emp.objects.filter(sal__lte=50000)
+    QLTO=Emp.objects.filter(deptno__in=('2','3'))
+    QLTO=Emp.objects.filter(pk__in=(11,))
+    QLTO=Emp.objects.filter(pk__in=(11,20))
+    QLTO=Emp.objects.filter(pk__in=('11','20'))
+    QLTO=Emp.objects.filter(ename__in=('Anjali','Sumithra'))
+    QLTO=Emp.objects.filter(sal__lt=50000)
+
 
     d={'Emp':QLTO}
     return render(request,'emp.html',d)
